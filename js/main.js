223940224f0581e9605fbe232554942244da7259
Salatiel Montero Gonzalez <salatiel.monteroglez@gmail.com>
2023-06-17 15:09:18 -0600

    feat(main): impl function to handle query params and redirect login

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@ let showPass = false;
 
 const handlePass = () => {
   showPass = !showPass;
-  const eyePassword = document.getElementById("eyePassword");
-  const inputPassword = document.getElementById("password");
-  inputPassword.setAttribute("type", showPass ? "text" : "password");
+  const eyePassword = document.getElementById('eyePassword');
+  const inputPassword = document.getElementById('password');
+  inputPassword.setAttribute('type', showPass ? 'text' : 'password');
   eyePassword.setAttribute(
-    "src",
-    `img/password-${showPass ? "off" : "on"}.svg`
+    'src',
+    `img/password-${showPass ? 'off' : 'on'}.svg`
   );
 };
 
+const handleQueryParams = () => {
+  const queryString = window.location.search;
+  console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  if (urlParams.has('username') && urlParams.has('password')) {
+    const username = urlParams.get('username');
+    const password = urlParams.get('password');
+    document.login.username.value = username;
+    document.login.password.value = password;
+    doLogin();
+  };
+};
+
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $('.owl-carousel').owlCarousel({
     // nav: true,
     items: 1.0001,
     loop: true,
@@ -22,4 +35,5 @@ $(document).ready(function () {
     autoplayTimeout: 5000,
     autoplayHoverPause: true,
   });
+  handleQueryParams();
 });

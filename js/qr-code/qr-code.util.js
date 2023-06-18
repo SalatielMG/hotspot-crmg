f697297a04cd46660918a330cb182f892722cc48
Salatiel Montero Gonzalez <salatiel.monteroglez@gmail.com>
2023-06-17 23:09:07 -0600

    feat(qr-code.util): handle qr-code success scan and show empty state

--- js/qr-code/qr-code.util.js
+++ js/qr-code/qr-code.util.js
@@ -1,34 +1,31 @@
 const ElementModalQR = document.getElementById('modalQr');
 const ElementCloseModalQR = document.getElementById('closeModalQr');
+const ElementSelectorCamera = document.getElementById('selectorCamera');
+const ElementContainerControlCamera = document.querySelector('.controls-camera-container')
+const ElementButtonGoBack = document.querySelector('.return-back')
+const ElementLoadingCamera = document.querySelector('.spinner');
+const ELEMENT_QR_CODE_WRAPPER = document.querySelector('.qr-code-wrapper');
+const ELEMENT_NOT_QR_CODE_WRAPPER = document.querySelector('.not-qr-code-wrapper');
 
-const html5QrCode = new Html5Qrcode("qr-reader");
+const html5QrCode = new Html5Qrcode('qr-reader');
+let cameras = [];
+let cameraActiveId = null;
+let statusQrCode = {
+  loading: true,
+  isEnabled: false
+}
+let secondsLeftGoBack = 20;
+let intervalIdGoBack = null;
+
 const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-  console.log('successCode', {
+  console.log('successCodeQr', {
     decodedText,
     decodedResult
   });
-  document.login.username.value = 'salatiel';
-  document.login.password.value = '123456';
-  window.location = `http://fichas.pruebas/login?dst=http://www.msftconnecttest.com/redirect&username=${document.login.username.value}&password=${document.login.password.value}`;
-  // doLogin();
-};
-const fetchLoginPost = () => {
-  try {
-    let formData = new FormData();
-    formData.append('username', document.sendin.username.value);
-    formData.append('password', document.sendin.password.value);
-    formData.append('dst', 'http://www.msftconnecttest.com/redirect');
-    formData.append('popup', true);
-    fetch('http://fichas.pruebas/login', {
-      method: 'post',
-      body: formData
-    }).then(() => {
-      console.log('Sesion iniciada correctamente');
-    }).catch(ErrorLogin)
-  } catch(error) {
-    ErrorLogin(error);
+  if (decodedText) {
+    window.location = decodedText;
   }
-}
+};
 
 const ErrorLogin = (error) => {
   console.error('error login', error);
@@ -38,33 +35,119 @@ const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
 const showModalQr = () => {
   ElementModalQR.style.visibility = 'visible';
-  // ElementFormLogin.style.visibility = 'hidden';
-    ElementCloseModalQR.addEventListener('click', closeModalQr);
+  ElementCloseModalQR.addEventListener('click', stopCamera);
 }
 
-const closeModalQr = () => {
-  ElementModalQR.style.visibility = 'hidden';
-  html5QrCode.stop().then((ignore) => {
-    // QR Code scanning is stopped.
-  }).catch((err) => {
-    // Stop failed, handle it.
-  });
-  // ElementFormLogin.style.visibility = 'visible';
-}
-
-const onInitScannerQrCode = () => {
-  showModalQr();
-  if (
-    navigator.userAgent.match(/Android/i) ||
-    navigator.userAgent.match(/iPhone/i)
-  ) {
-    html5QrCode.start({ facingMode: { exact: "environment"} }, config, qrCodeSuccessCallback);
-  } else {
-    html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+const stopCamera = async () => {
+  try {
+    await html5QrCode.stop();
+  } catch(error) {
+    console.error('error stopCamera', error);
   }
-  setTimeout(() => {
-    qrCodeSuccessCallback('', '');
-  }, 2000);
+}
+
+const hiddenControlsCamera = () => ElementContainerControlCamera.style.display = 'none';
+
+const visibleControlsCamera = () => ElementContainerControlCamera.style.display = 'flex';
+
+const handleGenerateSelectorCameras = () => {
+  if (cameras.length > 1) {
+    cameras.forEach(({
+      id,
+      label
+    }) => {
+      const option = document.createElement('option');
+      option.value = id;
+      option.text = label;
+      ElementSelectorCamera.add(option);
+    });
+  }
+}
+
+const intervalGoBack = () => {
+  intervalIdGoBack = setInterval(() => {
+    if (secondsLeftGoBack <= 0) {
+      goBack();
+    }
+    ElementButtonGoBack.textContent = `Regresar (${secondsLeftGoBack}s)`;
+    // ElementButtonGoBack.text = `Regresar (${secondsLeftGoBack}s)`;
+    secondsLeftGoBack--;
+    console.warn('secondsLeftGoBack', secondsLeftGoBack);
+  }, 1000);
+}
+
+const handleContainerQrCode = () => {
+  // ElementLoadingCamera.style.display = 'none';
+  ElementLoadingCamera.style.display = 'flex';
+  if (statusQrCode.loading) {
+    ELEMENT_QR_CODE_WRAPPER.style.display = 'flex';
+    ELEMENT_NOT_QR_CODE_WRAPPER.style.display = 'none';
+  } else if (!statusQrCode.isEnabled) {
+    ELEMENT_QR_CODE_WRAPPER.style.display = 'none';
+    ELEMENT_NOT_QR_CODE_WRAPPER.style.display = 'flex';
+    
+  } else {
+    ElementLoadingCamera.style.display = 'none';
+    ELEMENT_QR_CODE_WRAPPER.style.display = 'flex';
+    ELEMENT_NOT_QR_CODE_WRAPPER.style.display = 'none';
+  }
+}
+
+const getCameras = async () => {
+  try {
+    cameras = await Html5Qrcode.getCameras();
+    console.info('cameras', cameras);
+    if (cameras && cameras.length) {
+      cameraActiveId = cameras[0].id;
+      if (cameras.length > 1) {
+        visibleControlsCamera();
+        handleGenerateSelectorCameras();
+      }
+      await onInitScannerQrCode();
+    }
+    statusQrCode = {
+      loading: false,
+      isEnabled: cameras.length
+    }
+  } catch (error) {
+    console.error('error getCameras', error);
+    statusQrCode = {
+      loading: false,
+      isEnabled: false
+    }
+    intervalGoBack();
+  }
+  handleContainerQrCode();
+}
+
+const onInitScannerQrCode = async () => {
+  const status = html5QrCode.getState();
+  statusQrCode.loading = true;
+  handleContainerQrCode();
+  if (status !== Html5QrcodeScannerState.NOT_STARTED) { 
+    await stopCamera();
+  }
+  console.warn('status', status, Html5QrcodeScannerState);
+  await html5QrCode.start({ deviceId: { exact: cameraActiveId} }, config, qrCodeSuccessCallback);
+  statusQrCode.loading = false;
+  handleContainerQrCode();
 };
 
-onInitScannerQrCode();
+const changeSelected = async (event) => {
+  cameraActiveId = event.target.value;
+  await onInitScannerQrCode();
+};
+
+onInit = async () => {
+  secondsLeftGoBack = 20;
+  showModalQr();
+  await getCameras();
+  ElementSelectorCamera.addEventListener('change', changeSelected);
+}
+
+const goBack = () => {
+  clearInterval(intervalIdGoBack);
+  window.location = 'http://fichas.pruebas/login';
+}
+
+onInit();
